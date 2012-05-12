2c326fd6980e68ab089f650b43b10d091e22f735
user <jayunit100@gmail.com>
2012-05-11 21:37:29 -0400

    first

---
+++ src/rudolfweb/main_heroku.clj
@@ -0,0 +1,15 @@
+(ns rudolfweb.main-heroku
+  (:use ring.adapter.jetty)
+  (:use rudolfweb.routes)
+  (:require [compojure.handler :as handler]))
+; Ring provides that adapter to Jetty and the underlying HTTP requests and responses.
+; Difference to main-local.clj is that automatic reloading and browser stacktrace is not available under Heroku.
+
+(def app-heroku
+	(-> #'main-routes ; use the routes defined in main-routes 
+	    (handler/api) ; neccessary to access the form data in the paramter style way in the route definitions
+      (wrap-file "public"))) ; serve static files from directory 'public'
+
+(defn -main []
+  (let [port (Integer/parseInt (System/getenv "PORT"))]
+    (run-jetty app-heroku {:port port}))) ; run on Heroku!
