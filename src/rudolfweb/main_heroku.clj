c5266760470e6cd4e5ed2db97cdbae9f8b994bcc
Matt Fenwick <mfenwick100@gmail.com>
2012-05-14 08:23:14 -0400

    code formatting + whitespace: spaces only; two spaces initially, then one after; alignment of arguments; break long lines

--- src/rudolfweb/main_heroku.clj
+++ src/rudolfweb/main_heroku.clj
@@ -7,11 +7,11 @@
 ; Difference to main-local.clj is that automatic reloading and browser stacktrace is not available under Heroku.
 
 (def app-heroku
-	(-> #'main-routes ; use the routes defined in main-routes 
-	    (handler/api) ; neccessary to access the form data in the paramter style way in the route definitions
-      (wrap-file "public"))) ; serve static files from directory 'public'
+  (-> #'main-routes       ; use the routes defined in main-routes 
+   (handler/api)          ; neccessary to access the form data in the paramter style way in the route definitions
+   (wrap-file "public"))) ; serve static files from directory 'public'
 
 (defn -main []
   (let [_ (print "port is set ?" (System/getenv "PORT"))
         port (Integer/parseInt (System/getenv "PORT"))] ; Watch out ! PORT is a string ! 
-    (run-jetty app-heroku {:port port}))) ; run on Heroku!
+    (run-jetty app-heroku {:port port})))               ; run on Heroku!
