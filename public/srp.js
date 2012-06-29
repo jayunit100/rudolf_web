367966ea4619615ac1c06066b324d19563545731
Matt Fenwick <mfenwick100@gmail.com>
2012-06-29 09:01:45 -0400

    refactor srp.js according to current style conventions, with the help of jshint:
     - global variables
    
     - move variable declarations to top of containing function
    
     - remove multiple declarations of same variable
    
     - use === for equality check
    
     - insert blank lines for readability
    
     - put { and } after all ifs and elses
    
     - move constant declaration out of loop

--- public/srp.js
+++ public/srp.js
@@ -1,41 +1,58 @@
-var numposts=50;
-var numchars=100;
+
 function showrecentposts(json) {
+  var entry, posttitle, posturl, 
+      i, k, postdate, postauthor,
+      postcontent, 
+      re = /<\S[^>]*>/g,
+      numposts = 50,
+      numchars = 100;
+
   // start a loop
   // in this loop we get the entry from the feed and parse it
-  for (var i = 0; i < numposts; i++) {
+  for (i = 0; i < numposts; i++) {
     // get entry i from feed
-    var entry = json.feed.entry[i];
+    entry = json.feed.entry[i];
+    
     // get the posttitle
-    var posttitle = entry.title.$t;
+    posttitle = entry.title.$t;
+    
     // get the post url
     // check all links for the link with rel = alternate
-    var posturl;
-    if (i == json.feed.entry.length) break;
-    for (var k = 0; k < entry.link.length; k++) {
-      if (entry.link[k].rel == 'alternate') {
+    if (i === json.feed.entry.length) {
+      break;
+    }
+    for (k = 0; k < entry.link.length; k++) {
+      if (entry.link[k].rel === 'alternate') {
         posturl = entry.link[k].href;
         break;
       }
     }
+    
     // get the postdate, take only the first 10 characters
-    var postdate = entry.published.$t.substring(0,10);
+    postdate = entry.published.$t.substring(0,10);
+    
     // get the post author
-    var postauthor = entry.author[0].name.$t;
+    postauthor = entry.author[0].name.$t;
+    
     // get the postcontent
     // if the Blogger-feed is set to FULL, then the content is in the content-field
     // if the Blogger-feed is set to SHORT, then the content is in the summary-field
     if ("content" in entry) {
-      var postcontent = entry.content.$t;}
-    else
-    if ("summary" in entry) {
-      var postcontent = entry.summary.$t;}
-    else var postcontent = "";
+      postcontent = entry.content.$t;
+    } else if ("summary" in entry) {
+      postcontent = entry.summary.$t;
+    } else {
+      postcontent = "";
+    }
+    
     // strip off all html-tags
-    var re = /<\S[^>]*>/g;
     postcontent = postcontent.replace(re, "");
+    
     // reduce postcontent to numchar characters
-    if (postcontent.length > numchars) postcontent = postcontent.substring(0,numchars);
+    if (postcontent.length > numchars) {
+      postcontent = postcontent.substring(0,numchars);
+    }
+    
     // display the results
     //document.write('<a href="'+ posturl + '">' +posturl+'</a><br>');
     $("#jay").append('<a href="'+ posturl + '">' +posturl+'</a><br></br>');
