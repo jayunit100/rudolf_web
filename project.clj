96a9b8da3ad58de5680a3f2175a63af06c91a094
Matt Fenwick <mfenwick100@gmail.com>
2014-09-22 15:09:25 -0400

    clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject Clojure-Webapp-Example "1.0.0-SNAPSHOT"
   :description "A simple Clojure webapp that shows how to use Jetty, Ring, Compojure and Hiccup to build a web application."
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring "1.1.1"]
                  [compojure "0.6.4"]
